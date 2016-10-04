bd1cfad3d09327d3005e21bc53e8488737bd2786
mayconn <mayconnsuper1064@gmail.com>
2016-10-04 01:12:44 +0000

    Translated Model Event | Customized list of events in Admin

--- app/models/event.rb
+++ app/models/event.rb
@@ -9,5 +9,36 @@ class Event < ActiveRecord::Base
   # add a delete_<asset_name> method: 
   attr_accessor :delete_logo
   before_validation { self.logo.clear if self.delete_logo == '1' }
+  
+  RailsAdmin.config do |config|
+    config.model 'Event' do
+      list do
+        sort_by :id
+        items_per_page 20
+        field :id do
+          column_width 40
+          sort_reverse false
+        end
+        field :name do
+          column_width 140
+        end
+        field :event_url do
+          column_width 200
+        end
+        field :brief do
+          column_width 90
+        end
+        field :case do
+          column_width 90
+        end
+        field :perfil do
+          column_width 90
+        end
+        field :unify do
+          column_width 90
+        end
+      end
+    end
+  end
     
 end

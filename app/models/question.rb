41b0e105d1c443e3fdf62070bd7c60a54275dd9a
mayconn <mayconnsuper1064@gmail.com>
2016-10-10 11:37:32 +0000

    Fix register of question

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,20 @@ class Question < ActiveRecord::Base
   has_many :answers
   RailsAdmin.config.excluded_models << "Answer"
   
+  RailsAdmin.config do |config|
+    config.model 'Question' do
+      list do
+        field :id
+        field :description
+        field :hide
+      end
+      edit do
+        field :id
+        field :description
+        field :hide
+        field :answer
+      end
+    end
+  end
+  
 end
